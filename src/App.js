ed532f0cc3bf97a2098002492207709528d5fedb
martinsideas <acadecoder@gmail.com>
2021-12-03 14:02:37 -0500

    Mobile Design.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './styles/styles.css'
+/*IMAGES*/
+import nftImage from './images/image-equilibrium.jpg'
+import avatar from './images/image-avatar.png'
+import iconEth from './images/icon-ethereum.svg'
+import iconClock from './images/icon-clock.svg'
+/*IMAGES*/
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    return(
+<>
+    <Card />
+    <Attribution />
+</>
+    );
+}
+function Card(){
+    return(
+        <div className="card">
+            <img src={nftImage} alt="NFT" />
+            <div className="card-content">
+                <h2>Equilibrium #3429</h2>
+                <p className="about">Our Equilibrium collection promotes balance and calm.</p>
+                <div className="eth">
+                    <div className="eth-price">
+                        <img src={iconEth} alt="eth icon" />
+                        <p>0.041 ETH</p>
+                    </div>
+                    <div className="days-left">
+                        <img src={iconClock} alt="clock icon" />
+                        <p>3 days left</p>
+                    </div>
+                </div>
+                <hr />
+                <div className="creator">
+                    <div className="img-bg">
+                        <img src={avatar} alt="avatar icon"/>
+                    </div>
+                    <p>Creation of <span>Jules Wyvern</span></p>
+                </div>
+            </div>
+        </div>
+    );
+}
+function Attribution(){
+    return(
+        <div className="attribution">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
+            Coded by <a href="https://github.com/martinsideas" target="_blank" rel="noreferrer">Martin The Coder</a>.
+        </div>
+    );
 }
 
 export default App;
